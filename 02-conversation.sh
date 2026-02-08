dd5cac1130ba32ab272445ff50a78dfc9bb7557e
gopinadh <agp8896@gmail.com>
2026-02-08 11:10:35 +0530

    conversation

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 echo "ramesh:: hi suresh, how are you?"
 echo "suresh:: hello ramesh, i am fine. How are you?"
 echo "ramesh:: what are you learning recently?"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=RAM
+PERSON2=LAXMAN
+
+echo "$PERSON1:: hi $PERSON2, how are you?"
+echo "$PERSON2:: hello $PERSON1, i am fine. How are you?"
+echo "$PERSON1:: what are you learning recently?"
+echo "$PERSON2:: I am learning shell  scripting"
